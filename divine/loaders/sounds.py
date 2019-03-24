7232f38bca4045e0162b8a4380491ca35f474897
Lizband <trash.block2@gmail.com>
2019-03-24 14:17:41 -0400

    Add sounds to loader

---
+++ divine/loaders/sounds.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.init()
+
+menu_select = pygame.mixer.Sound('sounds/menu_select.wav')
+# effect.play()
+# pygame.mixer.music.load('foo.mp3')
+# pygame.mixer.music.play(-1)
